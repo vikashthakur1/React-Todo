652bf7081582221ce252ca07943bdfc553b0bc05
Vikash <vikastsr@gmail.com>
2024-01-01 11:47:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
+
+function generateid(){
+  return Math.floor(Math.random()*100);
+}
 
 function App() {
+const [todos,setTodos]=useState([]);
+const [input, setinput]=useState("");
+
+const handleSubmit =() =>{
+  setTodos((todos)=>
+    todos.concat({
+      text: input,
+      id: generateid(),
+    })
+  );
+    setinput("");
+  
+  }
+
+  const removeTodo =(id)=>
+    setTodos((todos)=>todos.filter((t)=>t.id!==id))
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+   <h1 className="hadding">Todo</h1> <hr/>
+   <input type="text" value={input} onChange={(e)=>setinput(e.target.value)} placeholder="New Todo"/>
+   <button onClick={handleSubmit}>Submit</button>
+
+   <ul className="todos-list">
+   {todos.map(({text,id})=>(
+    <li kay={id} className="todo">
+      <span>{text}</span>
+      <button className="close" onClick={()=>removeTodo(id)}>X</button>
+    </li>)
+   )}
+
+   </ul>
+   </div>
   );
 }
 
